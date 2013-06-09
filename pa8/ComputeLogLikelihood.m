ff1538ca589d8f40087f0350875811269978a7d8
Ian Yang <me@iany.me>
2013-06-09 12:26:03 +0800

    pa8: ComputeLogLikelihood

--- pa8/ComputeLogLikelihood.m
+++ pa8/ComputeLogLikelihood.m
@@ -20,6 +20,7 @@ N = size(dataset,1); % number of examples
 K = length(P.c); % number of classes
 
 loglikelihood = 0;
+
 % You should compute the log likelihood of data as in eq. (12) and (13)
 % in the PA description
 % Hint: Use lognormpdf instead of log(normpdf) to prevent underflow.
@@ -28,3 +29,39 @@ loglikelihood = 0;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % YOUR CODE HERE
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if length(size(G)) == 2
+  G = repmat(G, [1, 1, K]);
+end
+
+numParts = size(G, 1);
+
+for i = 1:N
+  exampleP = 0;
+  example = squeeze(dataset(i, :, :));
+
+  for k = 1:K
+    logP = 0;
+
+    for o = 1:numParts;
+      O = example(o, :);
+      clg = P.clg(o);
+
+      if G(o, 1, k) == 0
+        logP += lognormpdf(O(1), clg.mu_y(k), clg.sigma_y(k));
+        logP += lognormpdf(O(2), clg.mu_x(k), clg.sigma_x(k));
+        logP += lognormpdf(O(3), clg.mu_angle(k), clg.sigma_angle(k));
+      else
+        parent = [1, example(G(o, 2, k), :)];
+        theta = reshape(clg.theta(k, :), 4, 3);
+        logP += lognormpdf(O(1), parent * theta(:, 1), clg.sigma_y(k));
+        logP += lognormpdf(O(2), parent * theta(:, 2), clg.sigma_x(k));
+        logP += lognormpdf(O(3), parent * theta(:, 3), clg.sigma_angle(k));
+      end
+    end
+
+    exampleP += P.c(k) * exp(logP);
+  end
+
+  loglikelihood += log(exampleP);
+end
