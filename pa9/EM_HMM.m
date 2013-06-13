4497f238cde7a608649dcc79f05eb9bd4e95a946
Ian Yang <me@iany.me>
2013-06-13 17:24:32 +0800

    pa9: EM_HMM

--- pa9/EM_HMM.m
+++ pa9/EM_HMM.m
@@ -44,6 +44,8 @@ P.clg.sigma_x = [];
 P.clg.sigma_y = [];
 P.clg.sigma_angle = [];
 
+PriorSIndex = arrayfun(@(e) e.marg_ind(1), actionData);
+
 % EM algorithm
 for iter=1:maxIter
   
@@ -57,8 +59,27 @@ for iter=1:maxIter
   
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
-  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  P.c = mean(ClassProb(PriorSIndex, :));
+
+  for p = 1:size(G, 1)
+    for k = 1:K
+      weights = ClassProb(:, k);
+      if G(p, 1) == 0 # Gaussian
+        [P.clg(p).mu_y(k), P.clg(p).sigma_y(k)] = FitG(poseData(:, p, 1), weights);
+        [P.clg(p).mu_x(k), P.clg(p).sigma_x(k)] = FitG(poseData(:, p, 2), weights);
+        [P.clg(p).mu_angle(k), P.clg(p).sigma_angle(k)] = FitG(poseData(:, p, 3), weights);
+      else # CLG
+        parent = squeeze(poseData(:, G(p, 2), :));
+        [Beta1, P.clg(p).sigma_y(k)] = FitLG(poseData(:, p, 1), parent, weights);
+        [Beta2, P.clg(p).sigma_x(k)] = FitLG(poseData(:, p, 2), parent, weights);
+        [Beta3, P.clg(p).sigma_angle(k)] = FitLG(poseData(:, p, 3), parent, weights);
+
+        P.clg(p).theta(k, :) = [Beta1(end), Beta1(1:3)', Beta2(end), Beta2(1:3)', Beta3(end), Beta3(1:3)'];
+      end
+    end
+  end
+
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   % M-STEP to estimate parameters for transition matrix
@@ -68,11 +89,13 @@ for iter=1:maxIter
   
   % Add Dirichlet prior based on size of poseData to avoid 0 probabilities
   P.transMatrix = P.transMatrix + size(PairProb,1) * .05;
-  
+
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
+  P.transMatrix += reshape(sum(PairProb), 3, 3);
+  P.transMatrix ./= repmat(sum(P.transMatrix, 2), 1, 3);
+
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
     
@@ -85,7 +108,35 @@ for iter=1:maxIter
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
+
+  sigma = permute(reshape([P.clg.sigma_y, P.clg.sigma_x, P.clg.sigma_angle], K, size(G, 1), 3), [2, 3, 1]);
+  mu = zeros(size(sigma));
+  for p = find(~G(:, 1))'
+    mu(p, 1, :) = P.clg(p).mu_y;
+    mu(p, 2, :) = P.clg(p).mu_x;
+    mu(p, 3, :) = P.clg(p).mu_angle;
+  end
+
+  partsWithParent = find(G(:, 1));
+
+  for i = 1:N
+    example = repmat(squeeze(poseData(i, :, :)), [1, 1, K]);
+
+    for p = partsWithParent'
+      for k = 1:K
+         theta = reshape(P.clg(p).theta(k, :), 4, 3);
+        parentIndex = G(p, 2);
+        parent = example(parentIndex, :, k);
+        mu(p, :, k) = [1, parent] * theta;
+      end
+    end
+
+    logP = arrayfun(@lognormpdf, example, mu, sigma);
+    logP = squeeze(sum(sum(logP, 1), 2));
+
+    logEmissionProb(i, :) = logP';
+  end
+
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
     
@@ -103,6 +154,42 @@ for iter=1:maxIter
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % YOUR CODE HERE
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  for l = 1:L
+    a = actionData(l);
+
+    F = repmat(struct("var", [], "card", [], "val", []), length(a.marg_ind) + length(a.pair_ind) + 1, 1);
+
+    # Prior
+    F(1) = struct("var", [1], "card", [K], "val", log(P.c));
+
+    # Emission
+    for i = 1:length(a.marg_ind)
+      F(i + 1) = struct("var", [i], "card", [K], "val", logEmissionProb(a.marg_ind(i), :));
+    end
+
+    for i = 1:length(a.pair_ind)
+      F(i + 1 + length(a.marg_ind)) = struct("var", [i, i + 1], "card", [K, K], "val", log(P.transMatrix(:)'));
+    end
+
+    [M, PCalibrated] = ComputeExactMarginalsHMM(F);
+
+    singularProb = reshape([M.val], K, length(a.marg_ind))';
+    sumSingularProb = logsumexp(singularProb);
+    # normalize
+    singularProb -= repmat(sumSingularProb, 1, K);
+
+    ClassProb(a.marg_ind, :) = exp(singularProb);
+
+    # Because clique tree is a chain, it is gurantee Si-1,Si appear once and only once.
+    for i = 1:length(PCalibrated.cliqueList)
+      clique = PCalibrated.cliqueList(i);
+      index = min(clique.var);
+      PairProb(a.pair_ind(index), :) = exp(clique.val .- logsumexp(clique.val));
+    end
+
+    loglikelihood(iter) += logsumexp(PCalibrated.cliqueList(1).val);
+  end
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
